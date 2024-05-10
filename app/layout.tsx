08e819e14e8b8aaa46517eaeb86f217f18d3d730
0teklee <leetekwoo@gmail.com>
2024-05-10 16:41:13 +0900

    fix: meta tag og added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,21 @@ import type { Metadata } from "next";
 
 import { Noto_Sans_KR } from "next/font/google";
 
-
 import "./globals.css";
 import { clsx } from "clsx";
 
 import Header from "@/components/organism/Header";
-
 
 const notoSansKR = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lee Tekwoo - Portfolio",
   description: "FE Dev. 이택우 포트폴리오",
+  openGraph: {
+    title: "Lee Tekwoo - Portfolio",
+    description: "FE Dev. 이택우 포트폴리오",
+    images: ["/images/img179.jpg"],
+  },
 };
 
 export default function RootLayout({
