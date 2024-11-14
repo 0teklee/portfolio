e5b10023a7c07721573b9d79819d60e129debe06
0teklee <leetekwoo@gmail.com>
2024-11-14 15:36:40 +0900

    refactor: code split CoinvestorList.tsx to be molecules + atom

---
+++ components/atom/NavLinkItems.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import cn from 'clsx';
+import Row from "@/components/ui/Row";
+
+const LinkItem = ({ href, title, dateRange, className = '' }:{
+    href: string;
+    title: string;
+    dateRange: string;
+    className?:  string;
+}) => (
+    <Link href={href}>
+        <Row
+            classNames={cn(
+                `group cursor-pointer`,
+                `justify-between items-center w-full px-3 py-3`,
+                `border-b border-gray-200`,
+                `text-md font-medium`,
+                `hover:text-blue-500`,
+                `transition-all duration-200 ease-in-out`,
+                className
+            )}
+        >
+            <p>{title}</p>
+            {dateRange && <p>{dateRange}</p>}
+        </Row>
+    </Link>
+);
+
+export default LinkItem;
