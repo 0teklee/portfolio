1ca643be0c24e1c4ea67146bc651d7369c8cdaba
0teklee <leetekwoo@gmail.com>
2024-11-24 22:01:19 +0900

    fix: requestLocale -> "ko"

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,7 +2,7 @@ import { getRequestConfig } from "next-intl/server";
 
 import { routing } from "@/i18n/routing";
 
-export default getRequestConfig(async ({ requestLocale = "en" }) => {
+export default getRequestConfig(async ({ requestLocale = "ko" }) => {
   let locale = await requestLocale;
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
